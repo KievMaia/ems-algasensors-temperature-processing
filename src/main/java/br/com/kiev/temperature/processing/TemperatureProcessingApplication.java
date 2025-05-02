05a433d3ebcf55349c762945b18fb5d300949839
p0660061 <kiev.maia@portoseguro.com.br>
2025-05-02 18:24:20 -0300

    Ajusta os nomes dos pacotes.

--- src/main/java/br/com/kiev/temperature/processing/TemperatureProcessingApplication.java
+++ src/main/java/br/com/kiev/temperature/processing/TemperatureProcessingApplication.java
@@ -1,4 +1,4 @@
-package br.com.kiev.temperatureprocessing;
+package br.com.kiev.temperature.processing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/br/com/kiev/temperature/processing/TemperatureProcessingApplicationTests.java
+++ src/test/java/br/com/kiev/temperature/processing/TemperatureProcessingApplicationTests.java
@@ -1,4 +1,4 @@
-package br.com.kiev.temperatureprocessing;
+package br.com.kiev.temperature.processing;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
